8b5d54d0853de440d962bd25f862f0f248e7506e
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-03-10 18:07:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ujian_advance/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ujian_advance.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
